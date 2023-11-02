c03430b2872ecf130b26ca0d2c991ee15af809d8
VarunRane <126015896+VarunRane@users.noreply.github.com>
2023-11-02 16:58:43 +0530

    surveyformJS.js

--- HTML-CSS-JS/Survey form/surveyformJS.js
+++ HTML-CSS-JS/Survey form/surveyformJS.js
@@ -1,16 +1,16 @@
 const countrySelect = document.getElementById("country");
 
-        // Fetch country names from the REST Countries API
+        
         fetch("https://restcountries.com/v3.1/all")
         .then(response => response.json())
         .then(data => {
-        // Extract country names from the data
+        
         const countryNames = data.map(country => country.name.common);
     
-        // Sort the country names alphabetically
+       
         countryNames.sort();
     
-        // Populate the dropdown with sorted country names
+        
         countryNames.forEach(countryName => {
             const option = document.createElement("option");
             option.value = countryName;
